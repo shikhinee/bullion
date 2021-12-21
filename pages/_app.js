2fbaa0cc455e2103dc9a1af8736f2483c12b2e15
Jargalsaikhan Erdenetsetseg <thejargalsaikhan@gmail.com>
2021-12-21 17:39:13 +0800

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 // import { Provider } from "next-auth/client";
+import { Fragment } from "react";
 import ThemeLayout from "@/layouts/Theme";
 import { ActiveAnchorContextProvider } from "@/store/ActiveAnchor";
 import { ThemeContextProvider } from "@/store/ThemeContext";
@@ -8,20 +9,14 @@ import "@/sass/app.scss";
 const App = ({ Component, pageProps: { session, ...pageProps } }) => {
   const Layout = Component.Layout ? Component.Layout : Fragment;
 
-  const LayoutWithRef = () => {
-    return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    );
-  };
-
   return (
     // <Provider session={session}>
     <ActiveAnchorContextProvider>
       <ThemeContextProvider>
         <ThemeLayout>
-          <LayoutWithRef />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
         </ThemeLayout>
       </ThemeContextProvider>
     </ActiveAnchorContextProvider>
