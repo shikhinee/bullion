7da31993afbf90c1c9219ff658d61034b624c7ae
Jargalsaikhan Erdenetsetseg <thejargalsaikhan@gmail.com>
2021-12-21 16:30:34 +0800

    pdf

--- views/Landing/Hyfi/index.jsx
+++ views/Landing/Hyfi/index.jsx
@@ -1,6 +1,6 @@
 //Next, React (core node_modules) imports must be placed here
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 //import STORE from '@/store'
 
 //import COMPOSITES from '@/composites'
@@ -10,18 +10,23 @@ import Link from "next/link"
 import styles from "./Hyfi.module.scss";
 
 const Hyfi = (props) => {
-	 return (
-		 <div className={styles.container}>
-			 <Link href="https://res.cloudinary.com/keymaster123/image/upload/v1640066197/Bullion/Bullion_Ecosystem_Flow_Chart_ofkzqt.png">
-			<div className={styles.cta}>
-				 <h2 className={styles.hoverUnderline}>HyFi Ecosystem</h2>
-				 <div className={styles.arrow}>
-				 <Image src="/arrowlong.svg" width={130} height={50} layout="responsive"/>
-				 </div>
-		 	</div>
-			 </Link>
-		</div>
-	)
+  return (
+    <div className={styles.container}>
+      <Link href="bullion_ecosystem_flow_chart.pdf">
+        <div className={styles.cta}>
+          <h2 className={styles.hoverUnderline}>HyFi Ecosystem</h2>
+          <div className={styles.arrow}>
+            <Image
+              src="/arrowlong.svg"
+              width={130}
+              height={50}
+              layout="responsive"
+            />
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Hyfi;
