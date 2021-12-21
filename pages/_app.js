3dcc69c780962667415443ef3a1cc789e6a35af3
Jargalsaikhan Erdenetsetseg <thejargalsaikhan@gmail.com>
2021-12-21 08:00:53 +0800

    useless code

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,21 @@
-import { AnimatePresence, motion, AnimateSharedLayout } from "framer-motion";
-
-import { useRouter } from "next/router";
-
-import { Fragment, useRef, forwardRef, useImperativeHandle } from "react";
 // import { Provider } from "next-auth/client";
-
 import ThemeLayout from "@/layouts/Theme";
-
 import { ActiveAnchorContextProvider } from "@/store/ActiveAnchor";
 import { ThemeContextProvider } from "@/store/ThemeContext";
 
 import "@/sass/app.scss";
 
 const App = ({ Component, pageProps: { session, ...pageProps } }) => {
-  const router = useRouter();
   const Layout = Component.Layout ? Component.Layout : Fragment;
 
-  const ForwardedComponent = forwardRef((props, ref) => {
-    useImperativeHandle(ref, () => ({}));
-
-    return <Component {...props} {...pageProps} key={router.route} />;
-  });
-
-  ForwardedComponent.displayName = "ForwardedComponent";
-
   const LayoutWithRef = () => {
-    const componentRef = useRef();
     return (
       <Layout>
-        <ForwardedComponent ref={componentRef} />;
+        {
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        }
       </Layout>
     );
   };
